a9992e24c2671f46509fbaed3dc6889254f293d7
Andrei Hodorog <hodorog.andrei@gmail.com>
2015-04-09 10:23:38 +0000

    modified theme `JustWrite` version 1.0.7

--- wp-content/themes/justwrite/header.php
+++ wp-content/themes/justwrite/header.php
@@ -11,6 +11,25 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <?php wp_head(); ?>
+<link rel="apple-touch-icon" sizes="57x57" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/manifest.json">
+<link rel="shortcut icon" href="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/mstile-144x144.png">
+<meta name="msapplication-config" content="http://cdn.gazeta.info.ro/wp-content/uploads/2015/04/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
 </head>
 
 <body <?php body_class(); ?>>
