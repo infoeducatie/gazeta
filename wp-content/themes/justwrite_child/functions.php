d6544215447f55d3da13c753e73ad4b1525e378d
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-04-22 00:00:18 +0300

    Fix import

--- wp-content/themes/justwrite_child/functions.php
+++ wp-content/themes/justwrite_child/functions.php
@@ -4,7 +4,6 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
 }
 
 // Function for creating Widegets
