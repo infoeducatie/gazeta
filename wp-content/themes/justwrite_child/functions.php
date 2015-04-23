57f09a1239ff4419b20782939b76a2b74740c3cd
PressLabs <support@presslabs.com>
2015-04-23 20:29:23 +0000

    Merged changes from http://gazeta.info.ro on 04.23.2015

--- wp-content/themes/justwrite_child/functions.php
+++ wp-content/themes/justwrite_child/functions.php
@@ -6,22 +6,4 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
-// Function for creating Widegets
-function create_widget($name, $id, $description) {
-
-    register_sidebar(array(
-        'name' => __( $name ),
-        'id' => $id,
-        'description' => __( $description ),
-        'before_widget' => '<aside id="'.$id.'" class="widget %1$s %2$s">',
-		'after_widget' => '</aside>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>'
-    ));
-
-}
-
-// Create the actual widgets
-create_widget("Footer Widget", "footer-widget-2", "Displays in the footer of the site, below the copyright");
-
 ?>
--- wp-content/themes/justwrite_child/header.php
+++ wp-content/themes/justwrite_child/header.php
@@ -79,7 +79,7 @@
         
         <div class="search-wrap nobs">
         	<form role="search" id="header-search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-            	<input type="submit" class="search-submit" value="<?php _e( 'Search', 'acosmin' ); ?>" />
+            	<input type="submit" class="search-submit" value="<?php _e( 'Caută', 'acosmin' ); ?>" />
             	<div class="field-wrap">
 					<input type="search" class="search-field" placeholder="<?php _e( 'introdu cuvinte cheie ...', 'acosmin' ); ?>" value="<?php get_search_query(); ?>" name="s" title="<?php _e( 'Căutare:', 'acosmin' ); ?>" />
 				</div>
