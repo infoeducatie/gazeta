1a9dd2803246e455cb5259be9ce89003bb3c0786
Andrei Hodorog <hodorog.andrei@gmail.com>
2015-05-04 22:20:01 +0100

    Added footer ads widget area in which you can add Agora logo & other stuff

--- wp-content/themes/hueman_child/functions.php
+++ wp-content/themes/hueman_child/functions.php
@@ -12,4 +12,18 @@ function theme_enqueue_styles() {
 
 // load languages from child theme
 load_theme_textdomain( 'hueman', get_stylesheet_directory() . '/languages' );
+
+function footerads_widget_init() {
+	register_sidebar( array( 
+		'name' => 'Footer Ads',
+		'id' => 'footer-ads-custom', 
+		'description' => "Footer ads area", 
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h3>',
+		'after_title' => '</h3>'
+	));
+}
+
+add_action('widgets_init', 'footerads_widget_init');
 ?>
